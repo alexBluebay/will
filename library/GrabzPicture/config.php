e59ab77eca6f1a1d68c22ed488b3836e34a6bf7f
alexBluebay <alexandru.andries@bluebay.ro>
2014-10-09 18:14:38 +0300

    ok

---
+++ library/GrabzPicture/config.php
@@ -0,0 +1,8 @@
+<?php
+//Get this value from http://grabz.it/api
+$grabzItApplicationKey = "ZWQ2MTdkNTEzOWFlNDlhNTllMTk2MDdiY2Q0MGZkMmI=";
+//Get this value from http://grabz.it/api
+$grabzItApplicationSecret = "Pz8/Kz96Pz8/PzU/P3s/NDU/P0MdPz8AV1U/Pz8/Pz8=";
+//The absolute path that you have placed the handler.php on your website
+$grabzItHandlerUrl = "URL OF YOUR GrabzItHandler.php FILE (http://www.example.com/grabzit/handler.php)";
+?>
