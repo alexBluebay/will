14de4d0ea1c0ca9adcf72b180d314e540cce7383
alexBluebay <alexandru.andries@bluebay.ro>
2014-10-09 11:49:10 +0300

    test

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -10,7 +10,7 @@ class Admin_IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        //caca maca scris de copita lui eduard ada
+        //caca maca scris de copita lui eduard adadasdsa
         //session_destroy();
         $authModel = new Admin_Model_Components_AuthModel();
         $authModel->logged();
