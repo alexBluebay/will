3f5ae8c7a31c9fd4f2b204267d8496cf51bf2c08
alexBluebay <alexandru.andries@bluebay.ro>
2014-10-09 11:46:38 +0300

    commit din etbeans

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -10,7 +10,7 @@ class Admin_IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        //caca maca scris de copita lui eduard
+        //caca maca scris de copita lui eduard ada
         //session_destroy();
         $authModel = new Admin_Model_Components_AuthModel();
         $authModel->logged();
