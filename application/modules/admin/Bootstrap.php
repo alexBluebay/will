3223b3adb49421a9022cf887c33100fb1d77aca0
alexBluebay <alexandru.andries@bluebay.ro>
2014-10-09 11:33:49 +0300

    modul admin, primul commit

---
+++ application/modules/admin/Bootstrap.php
@@ -0,0 +1,206 @@
+<?php
+
+class  Admin_Bootstrap extends Zend_Application_Module_Bootstrap
+{
+    
+    public function _initSession()
+    {
+        new Zend_Session_Namespace();
+              
+    }
+
+    public function _initPreDispatch()
+    {
+        $this->bootstrap('frontController');
+        
+        $front = Zend_Controller_Front::getInstance();
+        
+        $plugin = new Admin_Model_Components_PreDispatch();
+        
+        $front->registerPlugin($plugin);
+        
+    }
+    
+    
+    
+
+    public function _initLayout()
+    {
+               
+       if(strpos($_SERVER['REQUEST_URI'], 'admin') !== FALSE) {
+
+            $options = array(
+                    'layout' => 'admin',
+                    'layoutPath' => APPLICATION_PATH . '/layouts/scripts'
+            );
+
+            Zend_Layout::startMvc($options);
+       }
+        
+    }
+    
+    public function _initRoutes()
+    {
+        $router = Zend_Controller_Front::getInstance()->getRouter();
+        
+        
+        $route = new Zend_Controller_Router_Route('admin/edit-link/:linkId',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'dashboard',
+                    'action' => 'edit-link',
+                    'linkId' => ''
+                    )
+                );
+        $router->addRoute('editLink', $route);
+        
+        
+        $route = new Zend_Controller_Router_Route('admin/view-link/:linkId',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'dashboard',
+                    'action' => 'view-link',
+                    'linkId' => ''
+                    )
+                );
+        $router->addRoute('viewLink', $route);
+        
+        
+        $route = new Zend_Controller_Router_Route('admin/all-links/',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'dashboard',
+                    'action' => 'all-links'
+                    )
+                );
+        $router->addRoute('allLinks', $route);
+        
+        $route = new Zend_Controller_Router_Route('admin/delete-link/:linkId',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'dashboard',
+                    'action' => 'del-link',
+                    'linkId' => ''
+                    )
+                );
+        $router->addRoute('delLink', $route);
+        
+        
+        $route = new Zend_Controller_Router_Route('admin/edit-category/:id',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'category',
+                    'action' => 'edit-category',
+                    'id' => ''
+                    )
+                );
+        $router->addRoute('editCategory', $route);
+        
+        
+        $route = new Zend_Controller_Router_Route('admin/delete-category/:id',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'category',
+                    'action' => 'del-category',
+                    'id' => ''
+                    )
+                );
+        $router->addRoute('delCategory', $route);
+        
+        $route = new Zend_Controller_Router_Route('admin/edit-advertising/:id',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'advertising',
+                    'action' => 'edit-advertising',
+                    'id' => ''
+                    )
+                );
+        $router->addRoute('editAdvertising', $route);
+        
+        $route = new Zend_Controller_Router_Route('admin/delete-advertising/:id',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'advertising',
+                    'action' => 'del-advertising',
+                    'id' => ''
+                    )
+                );
+        $router->addRoute('delAdvertising', $route);
+        
+        $route = new Zend_Controller_Router_Route('admin/edit-user/:id',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'users',
+                    'action' => 'edit-user',
+                    'id' => ''
+                    )
+                );
+        $router->addRoute('editUser', $route);
+        
+        $route = new Zend_Controller_Router_Route('admin/delete-user/:id',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'users',
+                    'action' => 'delete-user',
+                    'id' => ''
+                    )
+                );
+        $router->addRoute('delUser', $route);
+        
+        $route = new Zend_Controller_Router_Route('admin/edit-page/:id',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'pages',
+                    'action' => 'edit-page',
+                    'id' => ''
+                    )
+                );
+        $router->addRoute('editPage', $route);
+        
+        $route = new Zend_Controller_Router_Route('admin/edit-promo-link/:id',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'promo-links',
+                    'action' => 'edit-link',
+                    'id' => ''
+                    )
+                );
+        $router->addRoute('editPromoLink', $route);
+        
+        $route = new Zend_Controller_Router_Route('admin/delete-promo-link/:id',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'promo-links',
+                    'action' => 'delete-link',
+                    'id' => ''
+                    )
+                );
+        $router->addRoute('delPromoLink', $route);
+        
+        $route = new Zend_Controller_Router_Route('admin/add-promo-link/:id',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'promo-links',
+                    'action' => 'add-link',
+                    'id' => ''
+                    )
+                );
+        $router->addRoute('addPromoLink', $route);
+        
+        $route = new Zend_Controller_Router_Route('admin/logout',
+                array(
+                    'module' => 'admin',
+                    'controller' => 'index',
+                    'action' => 'logout',
+                    'id' => ''
+                    )
+                );
+        $router->addRoute('logout', $route);
+        
+    }
+    
+    
+
+
+}
+
--- application/modules/default/Bootstrap.php
+++ application/modules/default/Bootstrap.php
@@ -3,5 +3,137 @@
 class Default_Bootstrap extends Zend_Application_Module_Bootstrap
 {
 
+    
+   
+    public function _initRoutes()
+    {        
+        $router = Zend_Controller_Front::getInstance()->getRouter();
+        
+                               // Categorii nav
+        
+        $route = new Zend_Controller_Router_Route('categorii/',
+            array(
+                'controller' => 'categorii',
+                'action' => 'listare-categorii',
+            ));
+
+        $router->addRoute('listare_categorii', $route);
+        
+                               // Adaugare Link
+        
+        $route = new Zend_Controller_Router_Route('adauga/',
+            array(
+                'controller' => 'linkuri',
+                'action' => 'adauga-link',
+                
+            ));
+        
+        $router->addRoute('adauga_link', $route);
+                              
+                                // Termeni si conditii
+        
+        $route = new Zend_Controller_Router_Route('termeni/',
+            array(
+                'controller' => 'pagini',
+                'action' => 'termeni',
+                
+            ));
+        
+        $router->addRoute('termeni', $route);
+        
+                               // Contact
+        
+        $route = new Zend_Controller_Router_Route('contact/',
+            array(
+                'controller' => 'pagini',
+                'action' => 'contact',
+                
+            ));
+        
+        $router->addRoute('contact', $route);
+        
+                               // Servicii Will
+        
+        $route = new Zend_Controller_Router_Route('servicii/',
+            array(
+                'controller' => 'pagini',
+                'action' => 'servicii',
+                
+            ));
+        
+        $router->addRoute('servicii', $route);
+        
+                               // Avantaje Director Web
+        
+        $route = new Zend_Controller_Router_Route('avantaje/',
+            array(
+                'controller' => 'pagini',
+                'action' => 'avantaje',
+                
+            ));
+        
+        $router->addRoute('avantaje', $route);
+        
+                               // Categorii
+        
+        $route = new Zend_Controller_Router_Route('categorii/:category/:categoryId',
+            array(
+                'controller' => 'categorii',
+                'action' => 'listare-categorii',
+                'category' => '',
+                'categoryId' => '',
+            ));
+        
+        $router->addRoute('listare_categorii', $route);
+        
+                               // Subcategorii
+        $route = new Zend_Controller_Router_Route('/categorii/subcategorii/:category/:categoryId',
+            array(
+                'controller' => 'categorii',
+                'action' => 'listare-subcategorii',
+                'category' => '',
+                'categoryId' => '',
+            ));
+        
+        $router->addRoute('listare_subcategorii', $route);
+        
+                                // Listare Linkuri
+
+        $route = new Zend_Controller_Router_Route('/listare-linkuri/:subcategory/:subcategoryId',
+            array(
+                'controller' => 'linkuri',
+                'action' => 'listare-linkuri',
+                'subcategory' => '',
+                'subcategoryId' => '',
+            ));
+        
+        $router->addRoute('listare_linkuri', $route);       
+        
+                                // Linkuri
+        
+        $route = new Zend_Controller_Router_Route('/link/:title/:linkId',
+                array(
+                    'controller' => 'linkuri',
+                    'action' => 'detalii-link',
+                    'title' => '',
+                    'linkId' => '',
+                ));
+        $router->addRoute('detalii_link', $route);
+        
+    
+    }
+    
+    public function _initLayout()
+    {
+        
+//        $options = array(
+//                'layout' => 'layout',
+//                'layoutPath' => APPLICATION_PATH . '/layouts/scripts'
+//        );
+//        
+//        Zend_Layout::startMvc($options);
+        
+    }
+    
 }
 
