4e64e9970a9e3bdac557f78b49abfed3290d4181
alexBluebay <alexandru.andries@bluebay.ro>
2014-10-10 09:09:29 +0300

    ok

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -21,35 +21,49 @@ class IndexController extends Zend_Controller_Action
         
     }
     
-    public function grabzItAction(){
+    public function grabzItAction()
+    {
         $this->_helper->layout->disableLayout();
         $this->_helper->viewRenderer->setNoRender(true);
         
-        $message = $_GET["message"];
-        $customId = $_GET["customid"];
-        $id = $_GET["id"];
-        $filename = $_GET["filename"];
-         
-        //Custom id can be used to store user ids or whatever is needed for the later processing of the
-        //resulting screenshot
+        $message = $_GET['message'];
+        $customId = $_GET['customid'];
+        $id = $_GET['id'];
+        $filename = $_GET['filename'];
         
-        require APPLICATION_PATH . '/../library/GrabzPicture/lib/GrabzItClient.class.php';
+        include APPLICATION_PATH  . '/../library/GrabzPicture/lib/GrabzItClient.class.php';
         
         $grabzIt = new GrabzItClient("ZWQ2MTdkNTEzOWFlNDlhNTllMTk2MDdiY2Q0MGZkMmI=",
                                      "Pz8/Kz96Pz8/PzU/P3s/NDU/P0MdPz8AV1U/Pz8/Pz8=");
         
         $result = $grabzIt->GetPicture($id);
-         
+        
         if (!$result)
         {
             return;
         }
-         
+        
+        $uploadPath = APPLICATION_PATH . '/../public/default/img/uploads/';
         //Ensure that the application has the correct rights for this directory.
-        file_put_contents("images" . DIRECTORY_SEPARATOR . $filename, $result);
+        file_put_contents($uploadPath . $id.'.jpg', $result);
+        
+        
     }
 
 
+    
+        public function listareSubcategoriiAction()
+    {
+        //: am nevoie de parametru id categorie
+        // : apelez functie din model categorii care imi returneaza toate subcategoriile
+        
+        $parentId = $this->_request->getParam('categoryId');
+        
+        $subcatModel = new Default_Model_Components_Categorii();
+        $subcats = $subcatModel->getSubCategorii($parentId);
+        
+        $this->view->subcategorii = $subcats;
+    } 
 
 
     
